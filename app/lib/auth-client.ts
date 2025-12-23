2cec2bff4d34694ee2c3cfea940d72d0fc6b6c26
maroofinagit <maroofali688@gmail.com>
2025-12-23 17:32:56 +0530

    Udate auth url

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -2,7 +2,6 @@ import { nextCookies } from "better-auth/next-js"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
     plugins: [nextCookies()],
 })
 
